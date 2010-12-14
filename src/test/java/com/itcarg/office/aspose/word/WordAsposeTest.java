7ae44e0b24161a4a004171631492cb6f4f9a3346
bauna <bauna@bauna.com.ar>
2010-12-14 14:31:18 -0300

    switch to testng that runs tests in different threads

--- src/test/java/com/itcarg/office/aspose/word/WordAsposeTest.java
+++ src/test/java/com/itcarg/office/aspose/word/WordAsposeTest.java
@@ -1,50 +1,51 @@
 package com.itcarg.office.aspose.word;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertNotNull;
+import static org.testng.AssertJUnit.assertTrue;
 
 import java.io.File;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
 
 public class WordAsposeTest {
     private static final Logger log = LoggerFactory.getLogger(WordAsposeTest.class);
-    
-    @Test
+
+    @Test(threadPoolSize = 4, invocationCount = 20, timeOut = 10000)
     public void testReplaceMapDocx() throws Exception {
-        testFile("/word/test_document.docx", "test.docx");
+        runFile("/word/test_document.docx", "test.docx");
     }
-    
-    @Test
+
+    @Test(threadPoolSize = 4, invocationCount = 20, timeOut = 10000)
     public void testReplaceMapDoc() throws Exception {
-        testFile("/word/test_document.doc", "test.doc");
+        runFile("/word/test_document.doc", "test.doc");
     }
-    
-    private void testFile(String fileName, String out) throws Exception {
+
+    private void runFile(String fileName, String out) throws Exception {
         log.info("start test for {}", fileName);
         WordAspose doc = new WordAspose(getClass().getResourceAsStream(fileName));
         assertNotNull(doc);
-        
+
         Map<String, String> props = new LinkedHashMap<String, String>();
-        
+
         for (int i = 1; i < 8; i++) {
             props.put("key" + i, "value" + i);
         }
         Map<String, Integer> replaceMap = doc.replaceMap(props);
-        
+
         File fileOut = new File(System.getProperty("java.io.tmpdir"), out);
         fileOut.delete();
         doc.saveAs(fileOut);
         assertTrue(fileOut.exists());
-        
+
         for (Map.Entry<String, Integer> entry : replaceMap.entrySet()) {
             log.info("checking replaces for: {}", entry);
-            assertTrue(props.containsKey(entry.getKey()));
+            AssertJUnit.assertTrue(props.containsKey(entry.getKey()));
             if ("key1".equals(entry.getKey())) {
                 assertEquals(2, entry.getValue().intValue());
             } else if ("key2".equals(entry.getKey())) {
