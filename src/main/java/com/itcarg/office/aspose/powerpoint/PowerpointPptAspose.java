0b0adc757d26c458312a2f3c31aa5dbb342e549b
bauna <bauna@bauna.com.ar>
2010-12-15 14:57:21 -0300

    - renamed to PowerpointPptAspose - extracted interface PowerpointHandler

--- src/main/java/com/itcarg/office/aspose/powerpoint/PowerpointPptAspose.java
+++ src/main/java/com/itcarg/office/aspose/powerpoint/PowerpointPptAspose.java
@@ -8,6 +8,8 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Map;
 
+import com.aspose.slides.Paragraph;
+import com.aspose.slides.Paragraphs;
 import com.aspose.slides.Placeholder;
 import com.aspose.slides.Placeholders;
 import com.aspose.slides.Presentation;
@@ -16,10 +18,10 @@ import com.aspose.slides.Slides;
 import com.aspose.slides.TextHolder;
 import com.itcarg.office.aspose.BaseAspose;
 
-public class PowerpointAspose extends BaseAspose {
+public class PowerpointPptAspose extends BaseAspose implements PowerpointHandler {
     private Presentation presentation;
 
-    public PowerpointAspose(InputStream slidesStream) {
+    public PowerpointPptAspose(InputStream slidesStream) {
         presentation = new Presentation(slidesStream);
     }
 
@@ -27,6 +29,10 @@ public class PowerpointAspose extends BaseAspose {
         return presentation;
     }
 
+    /* (non-Javadoc)
+     * @see com.itcarg.office.aspose.powerpoint.PowerpointHandler#replaceMap(java.util.Map)
+     */
+    @Override
     public void replaceMap(Map<String, String> properties) throws Exception {
         Slides slides = getPresentation().getSlides();
         for (int i = 0; i < slides.size(); i++) {
@@ -36,15 +42,24 @@ public class PowerpointAspose extends BaseAspose {
                 Placeholder placeholder = placeholders.get(j);
                 if (placeholder instanceof TextHolder) {
                     TextHolder textHolder = (TextHolder) placeholder;
-                    for (Map.Entry<String, String> entry : properties.entrySet()) {
-                        textHolder.setText(textHolder.getText().replaceAll("\\$" + entry.getKey(),
-                                entry.getValue()));
+                    Paragraphs paragraphs = textHolder.getParagraphs();
+                    for (int k = 0; k < paragraphs.size(); k++) {
+                        Paragraph paragraph = paragraphs.get(k);
+                        for (Map.Entry<String, String> entry : properties.entrySet()) {
+                            paragraph.setText(paragraph.getText()
+                                    .replaceAll("\\$" + entry.getKey(),
+                                            entry.getValue()));
+                        }
                     }
                 }
             }
         }
     }
 
+    /* (non-Javadoc)
+     * @see com.itcarg.office.aspose.powerpoint.PowerpointHandler#saveAs(java.io.File)
+     */
+    @Override
     public void saveAs(File filename) throws IOException {
         OutputStream out = null;
         try {
